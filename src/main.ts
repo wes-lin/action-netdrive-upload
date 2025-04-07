c018a3bac67a10688a0a78dc84334e638e1b2d7e
wesl <oncwnuOQP1_2ToE8k4HJTp6YUjFY@git.weixin.qq.com>
2025-04-08 00:15:50 +0800

    chore: 修订netdrive-sdk

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,25 @@
 import { env } from "process";
-import { getInput} from "@actions/core";
-import { FileTokenStore, LanZouYClient } from '@netdrive-sdk/ilanzou'
-import * as fs from 'node:fs'
-import * as crypto from 'crypto'
-import path from 'node:path'
+import { getInput } from "@actions/core";
+import { FileTokenStore, LanZouYClient } from "@netdrive-sdk/ilanzou";
+import * as fs from "node:fs";
+import * as crypto from "crypto";
+import path from "node:path";
 
 async function run() {
-  console.log('Available INPUT env vars:');
-  Object.keys(env)
-    .filter(k => k.startsWith('INPUT_'))
-    .forEach(k => console.log(`${k}=${process.env[k]}`));
-  // const files = getInput('files', { required: true })
-  const driveType = getInput('driveType ', { required: true })
-  // const username = getInput('user-name', { required: true });
-  // const passowrd = getInput('password', { required: true });
+  const files = getInput("files", { required: true });
+  const driveType = getInput("drive-type", { required: true });
+  const username = getInput("username", { required: true });
+  const password = getInput("password", { required: true });
+  const client = new LanZouYClient({
+    username,
+    password,
+  });
+  const res = await client.ensureFolderPath("店群助手/1.0.0");
+  console.log(res);
+  const res1 = await client.ensureFolderPath("店群助手/1.0.1");
+  console.log(res1);
+  const res2 = await client.ensureFolderPath("店群助手/1.0.3/xxx");
+  console.log(res2);
   // const folderId = getInput('folde-id');
 }
 
