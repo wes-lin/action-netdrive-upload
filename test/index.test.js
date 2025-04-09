ded4d8401e6200f4b76fd55ea4153295d4f857ee
wesl <oncwnuOQP1_2ToE8k4HJTp6YUjFY@git.weixin.qq.com>
2025-04-10 00:07:11 +0800

    fix: 修复测试脚本

--- test/index.test.js
+++ test/index.test.js
@@ -1,18 +1,20 @@
-const fs = require('fs')
-const crypto = require('crypto')
-const path = require('path')
+const fs = require("fs");
+const crypto = require("crypto");
+const path = require("path");
 
 const generateRandomFile = () => {
-  const size = 1024 * 1024 // 1MB
-  const randomData = crypto.randomBytes(size) // 生成随机数据
-  const filePath = `.cache/${crypto.randomUUID()}.bin`
-  const dirPath = path.dirname(filePath)
+  const size = 1024 * 1024; // 1MB
+  const randomData = crypto.randomBytes(size); // 生成随机数据
+  const filePath = `.cache/${crypto.randomUUID()}.bin`;
+  const dirPath = path.dirname(filePath);
   if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath, { recursive: true })
+    fs.mkdirSync(dirPath, { recursive: true });
   }
   // 将随机数据写入文件
-  fs.writeFileSync(filePath, randomData)
-  return filePath
-}
+  const stream = fs.createWriteStream(filePath);
+  stream.write(randomData);
+  stream.end();
+  return filePath;
+};
 
-generateRandomFile()
+generateRandomFile();
